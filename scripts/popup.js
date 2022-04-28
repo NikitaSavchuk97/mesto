5edee26b90fe1c48cc6779f8df9466363a42ee21
NikitaSavchuk97 <nikita.savchuk@gmail.com>
2022-04-28 09:36:51 +0300

    полностью сделал ПР4, ожидаю замечаний ревьюера

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,17 +1,39 @@
+
 const popupOpenBtn = document.querySelector('.profile-info__edit-button');
 const popup = document.querySelector('.popup');
 const popupClose = document.querySelector('.popup__close');
 
 popupOpenBtn.addEventListener ('click', function() {
-    console.log('rabotaet');
     togglePopup();
 });
 
 popupClose.addEventListener ('click', function() {
-    console.log('pochti rabotaet');
     togglePopup();
 });
 
 function togglePopup () {
     popup.classList.toggle('popup_active');
 }
+
+
+
+let formElement = document.querySelector('.popup__container');
+let nameInput = formElement.querySelector('.popup__name');
+let jobInput = formElement.querySelector('.popup__about');
+
+let nameInfo = document.querySelector('.profile-info__name');
+let jobInfo = document.querySelector('.profile-info__title');
+
+nameInput.value = nameInfo.textContent;
+jobInput.value = jobInfo.textContent;
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    document.querySelector('.profile-info__name').innerHTML = nameInput.value;
+    document.querySelector('.profile-info__title').innerHTML = jobInput.value;
+    togglePopup();
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
+
+
